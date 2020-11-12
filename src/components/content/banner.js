4a416db9ba420ace19e0e5eb8c6c6e8a8d083d48
SpaceArk& <spacearkchief@gmail.com>
2020-11-13 03:10:15 +0700

    fixed home page added auto slidebanner

--- src/components/content/banner.js
+++ src/components/content/banner.js
@@ -22,11 +22,47 @@ import four from '../../assets/banner/4.jpg'
             i = (i + 1) % slides.length;
            slides[i].classList.add('active');
        }
+       function SlidePrev() {
+        slides[i].classList.remove('active');
+         i = (i - 1 + slides.length) % slides.length;
+        slides[i].classList.add('active');
+    }
        const next = document.querySelector('#nextButton');
+       const prev = document.querySelector('#prevButton')
+
        console.log(next)
        next.addEventListener('click', function() {
-           Slide();  
+           Slide(); SlideText()
+
        })
+       prev.addEventListener('click', function() {
+           SlidePrev()
+           SlideTextPrev()
+       } )
+
+       const bannerContentBx = document.querySelector('.banner-contentBx');
+       const slidesText = bannerContentBx.getElementsByTagName('div');
+       console.log(bannerContentBx);
+       let j = 0;
+    
+       function SlideText() {
+           slidesText[j].classList.remove('active');
+            j = (j + 1) % slidesText.length;
+           slidesText[j].classList.add('active');
+       }
+       function SlideTextPrev() {
+        slidesText[j].classList.remove('active');
+         j = (j - 1 + slidesText.length) % slidesText.length;
+        slidesText[j].classList.add('active');
+    }
+      
+       
+    const interval = setInterval(() => {
+        console.log('executed');
+        Slide()
+        SlideText()
+    }, 4000)
+      
       
    })
 
@@ -45,14 +81,36 @@ import four from '../../assets/banner/4.jpg'
 
         <section className="img-banner">
             <div className='imgBx'>
-                <img src={yon} className="active"/>
-                <img src={aww}/>
-                <img src={ias}/>
-                <img src={three}/>
-                <img src={four}/>
+                <img id='imgfirstslide' alt='' src={yon} className="active"/>
+                <img alt='' src={aww}/>
+                <img alt='' src={ias}/>
+                <img alt='' src={three}/>
+                <img alt='' src={four}/>
+            </div>
+            <div className='banner-contentBx'>
+                <div id='firstslide' className="active">
+                    <h2>Yon Mardiyono</h2>
+                    <p>National Athelte & CEO-Founder Ponks Sport</p>
+                </div>
+                <div id='secondslide' >
+                    <h2 >Empower Yourself</h2>
+                    <p>With XIOM Tennis Table Equipment Exceed your limit.</p>
+                </div>
+                <div id='thirdslide'>
+                    <h2>Indonesia All Star</h2>
+                    <p>Confidence isn't enough, XIOM Will Give You Power </p>
+                </div>
+                <div id='fourthslide'>
+                    <h2>Ready To Be A Champion?</h2>
+                    <p>XIOM Will Deliver it for you</p>
+                </div>
+                <div >
+                    <h2>xiom tennis table equipment</h2>
+                    <p>Provide You Complete Equipment For Your Training, Sparing And Competing</p>
+                </div>
             </div>
             <ul className="controls">
-                <li ></li>
+                <li id="prevButton" ></li>
                 <li id="nextButton"></li>
             </ul>
         </section>
--- src/components/content/productShow.js
+++ src/components/content/productShow.js
@@ -2,16 +2,16 @@ import React, { useEffect } from 'react';
 import Prev from '../../assets/prev.png'
 import Next from '../../assets/next.png'
 import Rubber from '../../assets/productShow/EURO.jpg'
-import Blade from '../../assets/productShow/HUGO_HAL1_1.png'
 import Table from '../../assets/productShow/BP-ITTF-PRO-9.png'
 import BNR from '../../assets/productShow/sensa100.png'
+import Blade from '../../assets/productShow/HUGO_HAL1_1.png'  
 import Fashion from '../../assets/productShow/fb3.png'
 import Accessory from '../../assets/productShow/i5.png'
 
 import '../../styles/ProductShow.css'
 
 const ProductShow = () => {
-
+    
     useEffect(() => {
         let thumbnails = document.getElementsByClassName('thumbnail')
         let activeImages = document.getElementsByClassName('ps_active')
@@ -24,63 +24,56 @@ const ProductShow = () => {
 				if (activeImages.length > 0){
 					activeImages[0].classList.remove('ps_active')
 				}
-				
-
 				this.classList.add('ps_active')
 				document.getElementById('featured').src = this.src
 			})
 		}
 
 
-		let buttonRight = document.getElementById('slide-right');
-		let buttonLeft = document.getElementById('slide-left');
+		// let buttonRight = document.getElementById('slide-right');
+		// let buttonLeft = document.getElementById('slide-left');
 
-		buttonLeft.addEventListener('click', function(){
-			document.getElementsByClassName('slider-ps').scrollLeft -= 180
-		})
+		// buttonLeft.addEventListener('click', function(){
+		// 	document.getElementsByClassName('slider-ps').scrollLeft -= 180
+		// })
 
-		buttonRight.addEventListener('click', function(){
-			document.getElementsByClassName('slider-ps').scrollLeft += 180
-		})
+		// buttonRight.addEventListener('click', function(){
+		// 	document.getElementsByClassName('slider-ps').scrollLeft += 180
+		// })
     })
 
     return ( <div className="content" id="content-wrapper">
         <div className="column">
+        <img alt='' id="featured" src={`${process.env.PUBLIC_URL}/product/blade/b1.png`}></img>
+        </div>
+        <div className="column">
             
-            <img id="featured" src={`${process.env.PUBLIC_URL}/product/blade/b1.png`}></img>
+            {/* <img id="featured" src={`${process.env.PUBLIC_URL}/product/blade/b1.png`}></img> */}
                 
                 <div className="textBx">
                 <h1>The Best Professional Equipment</h1>
                 <hr></hr>
                 <h3>Suits for All Your PlayStyle</h3>
                 <p>lorem ipsum dolor sir amet kuy</p>
-                <a className="link-button" href="#">See More</a>
+                <a className="link-button" href="#firstslide">See More</a>
                 <div id="slide-wrapper">
-                <img className="arrow" id="slide-left" src={Prev}></img>
+                
 
                 <div className="slider-ps">
-                    <img className="thumbnail ps_active" src={Rubber}></img>
-                    <img className="thumbnail" src={Blade}></img>
-                    <img className="thumbnail" src={Table}></img>
-                    <img className="thumbnail" src={BNR}></img>
-                    <img className="thumbnail" src={Fashion}></img>
-                    <img className="thumbnail" src={Accessory}></img>
+                    <img alt='' className="thumbnail ps_active" src={Rubber}></img>
+                    <img alt=''className="thumbnail" src={Blade}></img>
+                    <img alt='' className="thumbnail" src={Table}></img>
+                    <img alt='' className="thumbnail" src={BNR}></img>
+                    <img alt='' className="thumbnail" src={Fashion}></img>
+                    <img alt='' className="thumbnail" src={Accessory}></img>
                 </div>
 
-                <img className="arrow" id="slide-right" src={Next}></img>
+                
             </div>
                 </div>
                   
         </div>
 
-        <div className='column'>
-          
-
-          
-
-           
-
-        </div>
 
     </div> );
 }
