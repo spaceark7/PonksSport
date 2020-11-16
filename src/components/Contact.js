7bc133874fc7df8829921a0b0149601a2b1a649c
SpaceArk7 <spacearkchief@gmail.com>
2020-11-17 01:55:59 +0700

    adding contact section, about page is done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/Contact.css'
 const Contact = () => {
-    return ( <div>
-        <h2>This is Contact</h2>
-    </div> );
+    return ( 
+    <div className="container-fluid">
+        <div className="row">
+        <div className="col-md-6 col-sm-12">
+                <h2 className="contact-title">Ponks Sport Store</h2>
+                <p>is an Official Retail Store of XIOM PRODUCT KOREA</p>
+            </div>
+            <div className="col-md-6 col-sm-12">
+                <p className="title-desc">
+                    xiom is one of our product
+                </p>
+            </div>
+        </div>
+            
+        
+        </div>)
 }
  
 export default Contact;
