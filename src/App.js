29b803369b3cf065236ed346662d2c3bc3690a61
SpaceArk7 <spacearkchief@gmail.com>
2020-11-24 23:19:25 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Footer from './components/content/footer.js'
 import Nav from './components/content/nav.js';
 import Loader from './components/content/loader.js'
 
@@ -15,6 +15,7 @@ function App() {
       <Nav />
       {/* <Banner />
       <Product /> */}
+      <Footer />
     </div>
   );
 }
